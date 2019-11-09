17e809232fa27c35344ab50bcf9d187a76bfebe8
MerrryL <48694827+MerrryL@users.noreply.github.com>
2019-11-09 14:02:12 +0100

    Second commit

--- src/pages/professional.js
+++ src/pages/professional.js
@@ -19,7 +19,7 @@ const Professional= () => (
             <h3 className="display-5 m-5">Gardien de la Paix-Constatateur</h3>
 
             <p>For the Administration of the Town of Péruwelz<br/>
-            Since 14/09/2015 up to now <br/>
+            Since 14/09/2015, up to now <br/>
             I carry out prevention and security missions for the service of prevention and security.
             </p>
 
@@ -33,11 +33,35 @@ const Professional= () => (
 
                 </ListGroup.Item>
                 <ListGroup.Item>
-                    I keep ears and eyes wide open to anomalies
+                    I keep ears and eyes wide open to anomalies, perhaps dangers, answering them to my best.
 
                 </ListGroup.Item>
                 <ListGroup.Item>
+                    I collaborate with my team and different services, so that we can spot, communicate, analyze, prevent and fix ...<br/>
+                    deteriorations, dangers, incivilities or infrigments.
 
+                </ListGroup.Item>
+            </ListGroup>
+
+            <h3 className="display-5 m-5">Administrative Employee</h3>
+
+            <p>For the Administration of the Town of Péruwelz<br/>
+            2014-2015<br/>
+            In charge of the cartography and surveying the cemeteries of the town, and its informatization</p>
+
+            <ListGroup>
+                <ListGroup.Item>
+                    I determined the boundaries of the mission I was deemed to accomplish and I organized myself to reach the objectives.
+
+                </ListGroup.Item>
+                <ListGroup.Item>
+                    I ran through the cemeteries, drafted rough plans, took multiple pictures of every graves, and jot down about cryptic details.
+                </ListGroup.Item>
+                <ListGroup.Item>
+                    I organized folders and excel files according to the rows of graves, then inputed all the informations I could gather for the administrative personnal of the Etat-Civil service.
+                </ListGroup.Item>
+                <ListGroup.Item>
+                    I verified the informations before creating interactive maps to easen the communication between the administrative personnal (in the offices) and the gravediggers (in the field).
                 </ListGroup.Item>
             </ListGroup>
 
